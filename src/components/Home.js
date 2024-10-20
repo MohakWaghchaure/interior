dd660a710a6b6e89c5e7632f18259c7734a50751
Mohak Waghchaure <mohak11jan@gmail.com>
2024-10-20 16:14:05 -0400

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function Home(){
                     <div className="description-container">
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
                     </div>
-                    <div className="button-container">
+                    <div className="button-container ">
                         <button>Learn More</button>
                     </div>
                 </div>
